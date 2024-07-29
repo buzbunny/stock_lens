0a919dac9cb7162814d449a15865036b86570515
buzbunny <109228039+buzbunny@users.noreply.github.com>
2024-07-29 18:28:16 +0000

    added an app icon

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 import 'navbar.dart';
 import 'about_us.dart';
 import 'terms_and_conditions.dart';
-import 'login.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -14,8 +14,7 @@ class _SettingsPageState extends State<SettingsPage> {
   bool isBiometricEnabled = false;
   bool isDarkModeEnabled = false;
 
-
-Route createFadeRoute(Widget page) {
+  Route createFadeRoute(Widget page) {
     return PageRouteBuilder(
       pageBuilder: (context, animation, secondaryAnimation) => page,
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
@@ -33,60 +32,52 @@ Route createFadeRoute(Widget page) {
     );
   }
 
-void _showLogoutConfirmationDialog() {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        backgroundColor: Colors.grey[850], // Set background color to grey
-        title: Text(
-          'Confirm Logout',
-          style: TextStyle(color: Colors.white), // Set title text color to green
-        ),
-        content: Text(
-          'Are you sure you want to logout?',
-          style: TextStyle(color: Colors.white), // Set content text color to green
-        ),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text(
-              'Cancel',
-              style: TextStyle(color: Colors.red), // Set button text color to green
-            ),
+  void _showLogoutConfirmationDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.grey[850],
+          title: const Text(
+            'Confirm Exit',
+            style: TextStyle(color: Colors.white),
           ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-              Navigator.of(context).pushAndRemoveUntil(
-                createFadeRoute(LoginPage()),
-                (Route<dynamic> route) => false,
-              );
-            },
-            child: Text(
-              'Yes',
-              style: TextStyle(color: Colors.green), // Set button text color to green
-            ),
+          content: const Text(
+            'Are you sure you want to close the app?',
+            style: TextStyle(color: Colors.white),
           ),
-        ],
-      );
-    },
-  );
-}
-
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                'Cancel',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                // Close the dialog
+                Navigator.of(context).pop();
+                // Close the app
+                exit(0); // Use exit(0) to close the app
+              },
+              child: const Text(
+                'Yes',
+                style: TextStyle(color: Colors.green),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
         title: const Text(
           'Settings',
           style: TextStyle(
@@ -183,7 +174,7 @@ void _showLogoutConfirmationDialog() {
             trailing: const Icon(Icons.arrow_forward_ios, color: Colors.white),
             onTap: () {
               // Handle about us action
-              Navigator.of(context).push(createFadeRoute( AboutUsPage()));
+              Navigator.of(context).push(createFadeRoute(AboutUsPage()));
             },
           ),
           ListTile(
@@ -194,12 +185,12 @@ void _showLogoutConfirmationDialog() {
             trailing: const Icon(Icons.arrow_forward_ios, color: Colors.white),
             onTap: () {
               // Handle terms and conditions action
-              Navigator.of(context).push(createFadeRoute( TermsAndConditions()));
+              Navigator.of(context).push(createFadeRoute(TermsAndConditions()));
             },
           ),
           ListTile(
             title: const Text(
-              'Logout',
+              'Exit',
               style: TextStyle(color: Colors.white),
             ),
             trailing: const Icon(Icons.arrow_forward_ios, color: Colors.white),
